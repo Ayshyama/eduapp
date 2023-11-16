36264b9751a9fb58e654ef37c5cc4780d219d66c
serg <feelwork2077@gmail.com>
2023-11-16 12:05:07 +0100

    add models

--- app_accounts/models.py
+++ app_accounts/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from app_exercises.models import Exercise
+
+
+class CustomUser(AbstractUser):
+    bio = models.TextField(max_length=500, blank=True)
+    image = models.ImageField(upload_to='images/', blank=True)
+    email = models.EmailField(unique=True)
+    life = models.PositiveSmallIntegerField(default=10)
+    exercises_done = models.ManyToManyField(Exercise, through='UserProgress')
+    day_streak = models.PositiveSmallIntegerField(default=0)
+
+
+class UserExerciseConversation(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    message = models.TextField()
+    is_human = models.BooleanField(default=False)
+
+
+class UserProgress(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class UserStatistic(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    progress = models.PositiveSmallIntegerField(default=0)
--- app_exercises/models.py
+++ app_exercises/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500, blank=True)
+    image = models.ImageField(upload_to='images/')
+
+
+class Module(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500, blank=True)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500, blank=True)
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+
+
+class Exercise(models.Model):
+    name = models.TextField()
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    keywords = models.ManyToManyField('Keywords')
+    is_test = models.BooleanField(default=False)
+
+
+class Keywords(models.Model):
+    keyword = models.CharField(max_length=100)
+
+
+class TestAnswer(models.Model):
+    name = models.CharField(max_length=500)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    is_correct = models.BooleanField(default=False)
