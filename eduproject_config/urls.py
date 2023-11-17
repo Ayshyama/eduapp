507078fae01b502089f569a4a126cedd56cbe50d
Ayshyama@gmail.com <Ayshyama@gmail.com>
2023-11-17 10:52:29 +0100

    added signin/up html,css,js. Need to customize it more

--- eduproject_config/urls.py
+++ eduproject_config/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("app_base.urls")),
+    path("user/", include("app_accounts.urls")),
 ]
