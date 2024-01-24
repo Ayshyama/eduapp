2dd553fb89e9635f115f9356bc59bc9fc465fd15
Ayshyama@gmail.com <Ayshyama@gmail.com>
2024-01-24 09:44:59 +0100

    added back and front to dashboard with heatmap

--- app_dashboard/urls.py
+++ app_dashboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserDashboardView
+from .views import UserDashboardView, user_progress_json
 
 urlpatterns = [
     path('progress/', UserDashboardView.as_view(), name='user_progress'),
+    path('user-progress-json/', user_progress_json, name='user_progress_json'),
 ]
--- app_dashboard/views.py
+++ app_dashboard/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import Http404
+from django.db.models import Count
+from django.db.models.functions import TruncDate, TruncDay
+from django.http import Http404, JsonResponse
 from django.urls import reverse_lazy
 from django.views.generic import DetailView
 
-from app_accounts.models import CustomUser
+from app_accounts.models import CustomUser, UserProgress
 from app_base.nav_menu import menu1
 
 
@@ -21,4 +24,44 @@ class UserDashboardView(LoginRequiredMixin, DetailView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Dashboard'
         context['menu'] = menu1
+
+        user_progress = UserProgress.objects \
+            .filter(user=self.request.user) \
+            .annotate(date=TruncDay('datetime')) \
+            .values('date') \
+            .annotate(progress_count=Count('id')) \
+            .order_by('date')
+
+        progress_data = {progress['date'].strftime('%Y-%m-%d'): progress['progress_count'] for progress in user_progress}
+
+        context['progress_data'] = progress_data
+
         return context
+
+
+
+@login_required
+def user_progress_json(request):
+    user_progress = UserProgress.objects \
+        .filter(user=request.user) \
+        .annotate(date=TruncDay('datetime')) \
+        .values('date') \
+        .annotate(progress_count=Count('id')) \
+        .order_by('date')
+
+    progress_data = [
+        {
+            "date": progress['date'].strftime('%Y-%m-%d'),
+            "details": [
+                {
+                    "name": "Exercises done: ",
+                    "date": progress['date'],
+                    "value": progress['progress_count']
+                }
+            ]
+        }
+        for progress in user_progress
+    ]
+
+    return JsonResponse(progress_data, safe=False)
+
